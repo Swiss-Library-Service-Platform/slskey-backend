33d94612433234a03a9e9a8937ed4ce9366ee8a4
v1lling <sascha.villing@web.de>
2024-09-13 14:50:45 +0200

    Update todos

--- app/Jobs/TodoJobs.php
+++ app/Jobs/TodoJobs.php
@@ -8,32 +8,4 @@ TODO: Fix all FIXMEs before Go Live!
 TODO: set Jobs in Kernel.php
       
 ________________________________________________________________________
-Later, nice to have:
-
-TODO: Verifier
-      - mehr infos sehen im Portal
-      - vielleicht sogar als Bibliotheksuser??
-
-TODO: Cloud App
-      - Icon Jiggle Bug wenn geöffnet wird
-      - "No eduid" bereits in Liste anzeigen, nicht erst beim aktivieren
-
-TODO: Participage page: 
-      - Motivation / Vorteile aufzählen
-
-TODO: Test:
-      - for each job
-      - Add good test for Cloud App Auths
-      - Add good test for Alma Api Service (its all mocked)
-
-TODO: können wir mitbekommen wenn Switch edu-ID gelöscht wurde??
-
-TODO: SLskeyhistory bei Author den Namen anzeigen statt edu-ID/ user_identifier
-
-________________________________________________________________________
-Feedback Bibliotheken:
-
-TODO: User needs some kind of "affiliation"? e.g. welche Bibliothek, welche Abteilung, etc.
-      Beispiel: Aargau: sie wollen wissen, welche Bib genau einen User freigeschaltet hat
-
 */

e8bcb625dcd0a6a30f336b3d6ebba7c10931ae40
v1lling <sascha.villing@web.de>
2025-03-10 10:54:38 +0100

    Linting

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    
+
     protected function setUp(): void
     {
         parent::setUp();

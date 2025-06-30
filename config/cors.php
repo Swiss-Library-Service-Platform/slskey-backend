ee4896bf1bbe7a80ed2eef1a5c212527369905c4
v1lling <sascha.villing@web.de>
2025-06-30 14:13:21 +0200

    Update CORS configuration to specify allowed origins

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:4200', 'https://apps01.ext.exlibrisgroup.com'],
 
     'allowed_origins_patterns' => [],
 
